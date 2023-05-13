4b165197289b078662c6575ead529919cc22dd0d
Voroniyx <julian.ruge@icloud.com>
2023-05-13 16:36:44 +0200

    clean up

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
 export type ChannelType = 0 | 1 | 2 | 3 | 4 | 5 | 10 | 11 | 12 | 13 | 14 | 15;
 
 export enum Colors {
-    "red" = "[2;31m{{content}}",
-    "blue" = "[2;34m{{content}}",
-    "green" = "[2;32m{{content}}",
-    "yellow" = "[2;33m{{content}}",
-    "white" = "[2;37m{{content}}"
-}
+    RED = "\u001B[2;31m{{content}}",
+    BLUE = "\u001B[2;34m{{content}}",
+    GREEN = "\u001B[2;32m{{content}}",
+    YELLOW = "\u001B[2;33m{{content}}",
+    WHITE = "\u001B[2;37m{{content}}"
+};
 
 export enum TimestampTypes {
-    Date = "<t:unix:d>",
-    FullDate = "<t:unix:f>",
-    Time = "<t:unix:t>",
-    DescriptiveDate = "<t:unix:D>",
-    DescriptiveFull = "<t:unix:F>",
-    InAgo = "<t:unix:R>",
-    FullTime = "<t:unix:T>"
-}
+    DATE = "<t:unix:d>",
+    FULLDATE = "<t:unix:f>",
+    TIME = "<t:unix:t>",
+    DESCRIPTIVEDATE = "<t:unix:D>",
+    DESCRIPTIVEFULL = "<t:unix:F>",
+    INAGO = "<t:unix:R>",
+    FULLTIME = "<t:unix:T>"
+};
 
-export function generateAnsiText(content: string, color: Colors) {
+export function AnsiText(content: string, color: Colors) {
     let endColor;
     if (!content) throw new Error(`VoronsTools | You need to provied a content`)
     if (!color) endColor = `[2;37m{{content}}` //white
@@ -26,9 +26,8 @@ export function generateAnsiText(content: string, color: Colors) {
     let editColor = endColor.replace("{{content}}", content)
     let CodeBlock = `\`\`\`ansi\n${editColor}\n\`\`\``;
     return CodeBlock;
-}
-
-export function channelTypeConverter(channelType: ChannelType) {
+};
+export function ChannelTypeConverter(channelType: ChannelType) {
     const ChannelType = {
         0: "GuildText",
         1: "DM",
@@ -49,9 +48,9 @@ export function channelTypeConverter(channelType: ChannelType) {
         return ChannelType[channelType]
     }
     else return error;
-}
+};
 
-export function randomString(lenght: number) {
+export function RandomString(lenght: number) {
     if (!lenght) {
         throw new Error('VoronsTools | You must add a parameter of Number type(lenght)')
     }
@@ -64,9 +63,9 @@ export function randomString(lenght: number) {
         }
         return randomString
     }
-}
+};
 
-export function timestampGen(type: TimestampTypes, date:Date) {
+export function DiscordTimestamp(type: TimestampTypes, date:Date) {
     if (!type || !date) {
         throw new Error('VoronsTools | You must add a parameter to options of Timestamp Type and Date')
     }
@@ -79,4 +78,4 @@ export function timestampGen(type: TimestampTypes, date:Date) {
             return rDate
         }
     }
-}
+};
